40cdba688d539e834c3f4dd06c28c2e79c77d2a0
PB-Kronos <sandervdmolen2010@gmail.com>
2026-05-28 19:49:09 +0200

    made kiosk startup.bat

--- startup.bat
+++ startup.bat
@@ -1,6 +1,9 @@
-@echo true
-"C:\Program Files\WindowsApps\PythonSoftwareFoundation.PythonManager_26.2.240.0_x64__qbz5n2kfra8p0\python.exe" C:/Users/craftos/AppData/Roaming/CraftOS-PC/python/bridgefs.py
-"C:\Program Files\WindowsApps\PythonSoftwareFoundation.PythonManager_26.2.240.0_x64__qbz5n2kfra8p0\python.exe" C:/Users/craftos/AppData/Roaming/CraftOS-PC/python/execbridge.py
+@echo off
+set PYTHON="C:\Program Files\WindowsApps\PythonSoftwareFoundation.PythonManager_26.2.240.0_x64__qbz5n2kfra8p0\python.exe"
+set ROOT=C:\Users\craftos\AppData\Roaming\CraftOS-PC
 
-"C:\Program Files\CraftOS-PC\CraftOS-PC.exe"
-pause
+start "" /min %PYTHON% %ROOT%\python\bridgefs.py
+start "" /min %PYTHON% %ROOT%\python\execbridge.py
+start "" "C:\Program Files\CraftOS-PC\CraftOS-PC.exe"
+
+exit /b
