4d1e4a05bcdbf2674f6146807efb453435c1f18b
PB-Kronos <sandervdmolen2001@gmail.comgit config --global user.emailgit config --global user.email sandervdmolen2001@gmail.com>
2026-06-05 12:42:26 +0200

    Updated Startup to launch execbridge.py correctly

--- startup.bat
+++ startup.bat
@@ -1,13 +1,9 @@
 @echo off
-set PYTHON="C:\Program Files\WindowsApps\PythonSoftwareFoundation.PythonManager_26.2.240.0_x64__qbz5n2kfra8p0\python.exe"
+set PYTHON="C:\ProgramFiles\WindowsApps\PythonSoftwareFoundation.PythonManager_26.2.240.0_x64__qbz5n2kfra8p0\python.exe"
 set ROOT=C:\Users\craftos\AppData\Roaming\CraftOS-PC
 
-if exist "%ROOT%\python\*.py" (
-    for %%F in ("%ROOT%\python\*.py") do (
-        if exist "%%~fF" start "" /min %PYTHON% "%%~fF"
-    )
-)
-
+start "%PYTHON%" "%ROOT%\python\execbridge.py"
+start "%PYTHON%" "%ROOT%\python\bridgefs.py"
 start "" "C:\Program Files\CraftOS-PC\CraftOS-PC.exe"
 
 exit /b
