a33eeeff16d67b2a6e1fc67ab751d94a303bf55a
PB-Kronos <sandervdmolen2001@gmail.comgit config --global user.emailgit config --global user.email sandervdmolen2001@gmail.com>
2026-06-12 09:02:45 +0200

    Update upgrade.lua

--- pkg/ai/upgrade.lua
+++ pkg/ai/upgrade.lua
@@ -1,9 +1,9 @@
-if not fs.exists("/bin/ai.lua") then shell.run("wget raw.githubusercontent.com/PB-Kronos/CcShell/main/pkg/ai/ai.lua /bin/ai.lua") else
+if not fs.exists("/bin/ai.lua") then shell.run("wget https://raw.githubusercontent.com/PB-Kronos/CcShell/main/pkg/ai/ai.lua /bin/ai.lua") else
 fs.delete("/bin/ai.lua")
-shell.run("wget raw.githubusercontent.com/PB-Kronos/CcShell/main/pkg/ai/ai.lua /bin/ai.lua") end
+shell.run("wget https://raw.githubusercontent.com/PB-Kronos/CcShell/main/pkg/ai/ai.lua /bin/ai.lua") end
 print("Update system prompt?")
 local i = read()
 if i == "Y" or i == "y" or i == "yes" or i == "Yes" or "" then 
-if not fs.exists("/var/.ai_system") then shell.run("wget raw.githubusercontent.com/PB-Kronos/CcShell/main/pkg/ai/.ai_system /var/.ai_system") else
+if not fs.exists("/var/.ai_system") then shell.run("wget https://raw.githubusercontent.com/PB-Kronos/CcShell/main/pkg/ai/.ai_system /var/.ai_system") else
 fs.delete("/var/.ai_system")
-shell.run("wget raw.githubusercontent.com/PB-Kronos/CcShell/main/pkg/ai/.ai_system /var/.ai_system") end end
+shell.run("wget https://raw.githubusercontent.com/PB-Kronos/CcShell/main/pkg/ai/.ai_system /var/.ai_system") end end
